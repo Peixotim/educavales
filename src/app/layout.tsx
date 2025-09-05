43c20e7b75dda2beea8a9839411538a87556fe5b
Peixotim <pedropeixotovz@gmail.com>
2025-09-05 10:05:01 -0300

    feat: Adiciona o pixel do meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -29,6 +30,31 @@ export default function RootLayout({
           name="google-site-verification"
           content="p8j7IGmPd_8IsEFdVTNRjDD2pJvACQRk_mAqWs6bc1M"
         />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1843254712935806');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
+          <img
+            height="1"
+            width="1"
+            alt="facebook-script"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1843254712935806&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className={montserrat.className}>{children}</body>
     </html>
